0d231ae6ff2c569a64598e07e11b1264fec5c362
Gustavo Lourenco da Silva <gustavolsilva@outlook.com>
2024-06-28 19:19:07 -0300

    Deploy Inicial: Fundamentos de Pyhton - Prof Fernando Amaral

---
+++ 01.Fundamentos.py
@@ -0,0 +1,66 @@
+#%%
+# Isto é um comentário
+# Isto é outro comentário
+print("Olá mundo") # Isto é um comentário
+#%%
+'''
+Este é um comentário
+ou comentário continua aqui
+aqui ainda continua
+e aqui termina
+'''
+#%%
+print("Olá mundo")
+print(10)
+print(20.5)
+#%%
+print("Maça", 20 , 30.45)
+
+#%%
+print("Maçã", "Pera", "Uva", sep="-")
+#%%
+print("Maçã ", " Pera", end = " Fim", sep=' ')
+#%%
+print("Este é um texto longo, \n eu quero quebrar a linha")
+#%%
+print("Maçã", "Pera",  sep='\n')
+
+#%%
+# Tradicional, parecido com a Linguagem C
+print("A pontuação total de %s foi %s pontos" % ("Fernando", "10"))
+
+#%%
+# Utilização do .format (Desuso)
+print("A pontuação total de {} foi {} pontos".format("Fernando", "10"))
+
+#%%
+#Concatenação de Strings
+print("A pontuação total de" + " Fernando" + " foi" + " 10 " + "pontos")
+#%%
+#Fstring
+print(f"A pontuação total de {"Fernando"} foi {10} pontos")
+
+#%%
+# Atividade
+
+''' 1- Realize o print do seu nome completo, sua idade e sua altura utilizando um print para cada valor '''
+print(f"Gustavo")
+print(f"38")
+print(f"1.73")
+
+
+#%%
+''' 2- Realize o print do seu nome completo, sua idade e sua altura utilizando apenas um print para todos os
+valores '''
+print(f"{"Gustavo"} {38} {1.73} ")
+
+#%%
+'''
+3 - Realize o print de 3 números de sua escolha em um mesmo print, mas separados pelo caractere '-'
+'''
+print(45, 143.14, 7894, sep="-")
+
+#%%
+# Variaveis
+
+#%%
